d5e5a741ac69ff14db21e7828096fec95bacd1bc
Mark Middlemist <mark.middlemist@mercatosolutions.co.uk>
2022-11-30 15:23:32 +0000

    ID and Name display added

--- LogicWorkflowManagement/WorkflowEditor.Designer.cs
+++ LogicWorkflowManagement/WorkflowEditor.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxDefinition = new System.Windows.Forms.TextBox();
+            this.labelID = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxDefinition
+            // 
+            this.textBoxDefinition.Location = new System.Drawing.Point(12, 80);
+            this.textBoxDefinition.Multiline = true;
+            this.textBoxDefinition.Name = "textBoxDefinition";
+            this.textBoxDefinition.Size = new System.Drawing.Size(776, 314);
+            this.textBoxDefinition.TabIndex = 0;
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(8, 7);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(18, 15);
+            this.labelID.TabIndex = 1;
+            this.labelID.Text = "ID";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxID.Location = new System.Drawing.Point(53, 7);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(735, 23);
+            this.textBoxID.TabIndex = 2;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName.Location = new System.Drawing.Point(53, 36);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(735, 23);
+            this.textBoxName.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Definition";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(713, 416);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 6;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(632, 416);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 7;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // WorkflowEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.textBoxDefinition);
+            this.Name = "WorkflowEditor";
             this.Text = "WorkflowEditor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox textBoxDefinition;
+        private Label labelID;
+        private TextBox textBoxID;
+        private TextBox textBoxName;
+        private Label label1;
+        private Label label2;
+        private Button buttonUpdate;
+        private Button buttonCreate;
     }
 }
--- LogicWorkflowManagement/WorkflowEditor.cs
+++ LogicWorkflowManagement/WorkflowEditor.cs
@@ -15,14 +15,41 @@ namespace LogicWorkflowManagement
 {
     public partial class WorkflowEditor : Form
     {
+        LogicAppInterface _managementInterface;
+        LogicWorkflowResource? _targetWorkflow;
+
         public WorkflowEditor(LogicAppInterface managementInterface)
         {
+            this._managementInterface = managementInterface;
+
             InitializeComponent();
         }
 
         public WorkflowEditor(LogicAppInterface managementInterface, LogicWorkflowResource targetWorkflow)
         {
+            this._managementInterface = managementInterface;
+            this._targetWorkflow = targetWorkflow;
+
             InitializeComponent();
+
+            DisplayDefinition();
+        }
+
+        private void DisplayDefinition()
+        {
+            textBoxDefinition.Text = _targetWorkflow?.Data?.Definition?.ToString();
+            textBoxID.Text = _targetWorkflow?.Data?.Id?.ToString();
+            textBoxName.Text = _targetWorkflow?.Data?.Name?.ToString();
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
